a5746ab9fa11adc1bd537ba80c3f627f00716508
Cole Thesing <colethes@gmail.com>
2025-04-16 23:41:09 -0500

    Finished Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ContactCardComponent } from './contact-card/contact-card.component';
+import { Contact } from './Model/contact';
+// import { CONTACTS } from './Data/contact-data';
+import { filter } from 'rxjs';
+import { ContactsService } from './contacts.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactCardComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'assignment-4';
+title = 'contact-list'
+
+contacts: Contact[] = [];
+
+isAddingContact = false;
+
+constructor(private contactsService: ContactsService, private router: Router) {}
+  ngOnInit() {
+    this.contacts = this.contactsService.contacts();
+
+  this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: any) => {
+      this.isAddingContact = event.urlAfterRedirects === '/add';
+    });
+}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
+export const routes: Routes = [
+    {
+        path: 'add',
+        component: ContactFormComponent,
+        title: 'Add Contact'
+    },
+    {
+        path: 'edit/:email',
+        component: ContactFormComponent,
+        title: 'Edit Contact'
+    }
+
+];
